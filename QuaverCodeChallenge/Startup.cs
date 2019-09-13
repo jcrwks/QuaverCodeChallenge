d87e1db16e809fd2b42ae107fbfd2d5b01724f04
Joe Harmon <jharmon@blinfo.com>
2019-09-13 15:37:02 -0400

    V1.0 - Code Cleanup - Renamed Servcies to Services

--- QuaverCodeChallenge/Startup.cs
+++ QuaverCodeChallenge/Startup.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using QuaverCodeChallenge.Servcies;
+using QuaverCodeChallenge.Services;
 
 namespace QuaverCodeChallenge
 {
